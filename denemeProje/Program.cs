28f0f5bb754addb7a65f0bedf7ea10724e13abf7
muhlisbulus <muhlisbulus79@gmail.com>
2021-04-24 13:40:29 +0300

    Proje dosyası ekle.

---
+++ denemeProje/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace denemeProje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
